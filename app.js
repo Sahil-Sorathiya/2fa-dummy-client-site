5d5b83f4391d8e47409b374149452f6661a2ac52
Yasha-Thakur <thakuryasha24@gmail.com>
2023-05-10 18:02:12 +0530

    app.js added

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const { default: axios } = require('axios');
+const { log } = require('console');
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    const fileName = path.join(__dirname, 'index.html')
+    res.sendFile(fileName, (e) => {
+        if (e) {
+            return res.send(e);
+        }
+        else {
+            return console.log('Sent file')
+        }
+    })
+})
+
+app.post('/', async (req, res) => {
+    console.log(req.body);
+    try {
+        const userEmail = req.body.email;
+        // console.log(userEmail);
+        const apikey = process.env.APIKEY;
+        // console.log(apikey);
+        const redirectUrl = process.env.REDIRECT;
+        // console.log(redirectUrl);
+        const response = await axios.post('https://2fa-client.cyclic.app/sendotp', { userEmail, apikey, redirectUrl })
+        console.log(response.data);
+        if (response.data.success) {
+            return res.redirect(response.data.redirectTo)
+        }
+    } catch (error) {
+        if (error.response.data.error) {
+            return res.send(error.response.data)
+        }
+        return res.send(error)
+    }
+})
+
+app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
