b2c263916b65ed46a4b290be2639fd092af080ce
Sahil Sorathiya <67726151+Sahil-Sorathiya@users.noreply.github.com>
2023-05-12 09:33:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,30 +10,21 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     const fileName = path.join(__dirname, 'index.html')
-    res.sendFile(fileName, (e) => {
-        if (e) {
-            return res.send(e);
+    res.sendFile(fileName, (error) => {
+        if (error) {
+            return res.send(error);
         }
-        else {
-            return console.log('File has been sent!!')
-        }
+        return;
     })
 })
 
 app.post('/', async (req, res) => {
-    console.log(req.body);
     try {
-            const respons = await axios.get('https://2fa-client.cyclic.app/login');
-    console.log(respons);
-    console.log(respons?.data);
         const userEmail = req.body.email;
-        // console.log(userEmail);
         const apikey = process.env.APIKEY;
-        // console.log(apikey);
         const redirectUrl = process.env.REDIRECT;
-        // console.log(redirectUrl);
-        const response = await axios.post('https://2fa-client.cyclic.app/sendotp', { userEmail, apikey, redirectUrl })
-        console.log(response.data);
+        const domainName = process.env.DOMAINNAME;
+        const response = await axios.post('https://2fa-client.cyclic.app/sendotp', { userEmail, apikey, redirectUrl, domainName })
         if (response.data.success) {
             return res.redirect(response.data.redirectTo)
         }
